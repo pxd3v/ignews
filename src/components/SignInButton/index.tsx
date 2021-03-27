bbe1f8e6707e6769d1b93719b8a5dab9908a05c1
Pedro Lages <pedro.lages@hotmart.com>
2021-03-27 14:02:58 -0300

    update

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,16 @@
 import { FaGithub }  from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn, useSession } from 'next-auth/client'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
     const [session] = useSession();
-    console.log(session)
     return session ? (
             <button 
                 type="button"
                 className={styles.signInButton}
+                onClick={() => signOut()}
             >
                 <FaGithub color="#04d361"/>
                 {session.user.name}
